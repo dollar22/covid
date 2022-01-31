46cdb7bedb0f5e5be4ec113b18d2d455a9dc0a4c
dollar22 <90107665+dollar22@users.noreply.github.com>
2022-01-31 20:40:20 +0530

    Add files via upload

---
+++ covid.js
@@ -0,0 +1,23 @@
+import request from "request";
+import cheerio from "cheerio";
+import chalk from 'chalk';
+request('https://www.worldometers.info/coronavirus/', function (error, response, body) {
+ if(error){
+    console.error('error:', error); 
+ } else{
+     handlehtml(body);
+ }
+
+});
+
+function handlehtml(html){
+    let selectTools = cheerio.load(html);
+    let contentArr = selectTools("#maincounter-wrap span");
+    let totalcases = selectTools(contentArr[0]).text();
+    console.log(chalk.gray("Total cases: "+totalcases));
+    let recoverdcases = selectTools(contentArr[1]).text();
+    console.log(chalk.red("Total deaths: ",recoverdcases));
+    let deaths = selectTools(contentArr[2]).text();
+   console.log(chalk.green("Total recover: ", deaths));
+
+}
